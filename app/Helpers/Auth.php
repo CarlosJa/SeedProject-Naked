594ad9240d96dfc0ab03317d85057d9dbe482c02
Carlos Arias <hi@carlosarias.com>
2022-09-14 15:34:45 -0400

    updated the API module. To allow Internal and External API requests.

--- app/Helpers/Auth.php
+++ app/Helpers/Auth.php
@@ -3,29 +3,46 @@
 class Auth {
 
     public static function API() {
-        if ($_SERVER['REMOTE_ADDR'] != $_SERVER['SERVER_ADDR']) {
+        header('Access-Control-Allow-Origin: *');
+
+        // If its coming from an Ajax Request from inside the server return true and proceed.
+        if (! empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            return true;
+        }
+
+        // Anything outside the server will go through the API Key Process.
+        if ($_SERVER['REMOTE_ADDR'] != $_SERVER['SERVER_ADDR']) { # This happens if its outside the server.
             # If you want additional security for example. Authorization to Access the server you can use this.
             # enable SECUREAPI in config.php
-            if(SECUREAPI) {
+            if (SECUREAPI) {
                 $headers = apache_request_headers();
-                if($headers['X-Authorization'] != AUTHORIZATION) die('Access Denied');
+                if ($headers['X-Authorization'] != AUTHORIZATION) die('Access Denied');
             }
 
             # Requires Authorization
             $ApiKey = $_REQUEST['apikey'];
             $ApiUser = \Db::getRow("select * from sp_users_api where apikey = '{$ApiKey}'");
-            if(!$ApiUser) die('Unauthorized: invalid apikey');
+            if (!$ApiUser) {
+                $Return['code'] = '401';
+                $Return['msg'] = "Unauthorized: Api Invalid";
+                $Response = json_encode($Return);
+
+                header('Content-Type: ' . ($_GET['callback'] ? 'application/javascript' : 'application/json') . ';charset=UTF-8');
+                echo isset($_GET['callback']) ? "/**/ typeof {$_GET['callback']} === 'function' && {$_GET['callback']}($Response)" : $Response;
+                exit();
+            }
 
             #TODO: ////// Security Layers Below ////////
             #TODO: Record API Requests
             #TODO: Add API Limits
             #Todo: Check for Whitelist IP Address
 
-
+            ### Everything Passed Now proceed with the API Request.
             $Return['code'] = '1';
             $Return['userid'] = $ApiUser['userid'];
             return $Return;
         }
+
     }
 
 
@@ -33,7 +50,7 @@ class Auth {
         $logged = $_SESSION['login'];
         if ($logged == false) {
             session_destroy();
-            header('location: ./login');
+            header('location: ' . SITE_BASE . '/login');
             exit;
         }
 
